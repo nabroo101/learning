b6c2a0f9283b02dac2e6cd0b5d669edf74366b78
nabroo101 <124211807+nabroo101@users.noreply.github.com>
2023-02-01 17:06:18 -0500

    Create first-code.py

---
+++ first-code.py
@@ -0,0 +1,20 @@
+#!/usr/python3
+# this is our first script
+
+
+Number1 = input("enter number 1: ")
+Number1 = float(Number1)
+Number2 = input("enter number 2: ")
+Number2 = float(Number2)
+Total = Number1 + Number2
+if Total == 15 :
+    Number3 = input(" You are lucky , guess one more number : ")
+    Number3 = float(Number3)
+    Total2 = Total + Number3
+    if Total2 == 20:
+        print ( "YOU WIN!!")
+    else : 
+        print (" Thank you for playing:" + str(Total2))
+        
+else : 
+    print ("your total is : " + str(Total))
