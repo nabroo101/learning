95a21d647656867f1a7f0e5ebe5c386ab0096281
nabroo101 <124211807+nabroo101@users.noreply.github.com>
2023-02-01 17:09:02 -0500

    Update first-code.py

--- first-code.py
+++ first-code.py
@@ -6,7 +6,7 @@ Number1 = input("enter number 1: ")
 Number1 = float(Number1)
 Number2 = input("enter number 2: ")
 Number2 = float(Number2)
-Total = Number1 + Number2
+Total = Number1 + Number2 # 
 if Total == 15 :
     Number3 = input(" You are lucky , guess one more number : ")
     Number3 = float(Number3)
